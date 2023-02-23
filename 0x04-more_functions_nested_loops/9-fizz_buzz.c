2d4c2719ef5f76fa83d9fc99150df7888d6a9569
SafaFarah <kangjisook13@gmail.com>
2023-02-23 14:57:20 -0800

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,11 +13,7 @@ int main(void)
 	/* for loop*/
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 15 == 0)
-		{
-			printf("FizzBuzz");
-		}
-		else if (i % 3 == 0)
+		if (i % 3 == 0)
 		{
 			printf("Fizz");
 		}
@@ -25,11 +21,14 @@ int main(void)
 		{
 			printf("Buzz");
 		}
+		else if (i % 15 == 0)
+		{
+			printf("FizzBuzz");
 		else
 		{
 			printf("%d", i);
 		}
-		putchar(32);
+		printf(" ");
 	}
 	printf("\n");
 	return (0);
