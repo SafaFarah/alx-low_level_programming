2ab9b9ff876ed4f33cc09abd447602e8ac88c42c
SafaFarah <kangjisook13@gmail.com>
2023-02-25 10:31:56 -0800

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+int j;
+int k;
+int l;
+
+/* for loop*/
+for (i = 48 ; i <= 57 ; i++)
+{
+/* for loop*/
+for (j = 48 ; j < 57 ; j++)
+{
+/* for loop */
+for (k = 48 ; k <= 57 ; k++)
+{
+/* for loop*/
+for (l = 49 ; l <= 57 ; l++)
+{
+if (l >  j && k >= i)
+{
+putchar(i);
+putchar(j);
+putchar(32);
+putchar(k);
+			putchar(l);
+if (j == 56 && i == 57)
+	break;
+putchar(44);
+putchar(32);
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
