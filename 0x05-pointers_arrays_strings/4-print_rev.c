faa9607d9abb95eb33e8ec9de8cc79ad9255cc2a
SafaFarah <kangjisook13@gmail.com>
2023-02-28 05:15:46 -0800

    I've not got a reverse gear

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - a function that prints a string,
+ *@s: string pointer
+ */
+void print_rev(char *s)
+{
+	int count = 0;
+	int i = 0;
+	int j;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+		count++;
+	}
+	j = count - 1;
+	for (; j >= 0; j++)
+	{
+		_putchar(*(s + j));
+	}
+	_putchar('\n');
+}
+
+
