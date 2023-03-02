488adc149f60cff8356d95bf5ae33ae88f6ababf
SafaFarah <kangjisook13@gmail.com>
2023-03-02 07:34:52 -0800

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,16 +14,25 @@ char *_strncat(char *dest, char *src, int n)
 	int count = 0;
 	int k;
 	int l = 0;
+	int j = 0;
+	int length = 0;
 
 	while (*(dest + i) != '\0')
 	{
 		i++;
 		count++;
 	}
+	while (*(src + j) != '\0')
+	{
+		j++;
+		length++;
+	}
 	for (k = count; k < (count + n); k++)
 	{
 		dest[k] = src[l];
 		l++;
+		if (k > (count + length))
+			break;
 	}
 	return (dest);
 }
