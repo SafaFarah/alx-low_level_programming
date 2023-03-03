c9c54536676ea86e761dad4051bc7174e885fe13
SafaFarah <kangjisook13@gmail.com>
2023-03-03 05:49:41 -0800

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,40 +5,28 @@
  * @s1: string
  * @s2: string
  *
- * Return : number 
+ * Return : number
  */
- int _strcmp(char *s1, char *s2)
+int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
 	int count = 0;
-	int k;
 	int length = 0;
 	int j = 0;
-	int w = 0;
-	int total = 0;
-	int n = 0;
-	int total2 = 0;
-	int s = 0;
+	int w;
+	int n;
 
 	while (*(s1 + i) != '\0')
 	{
 		i++;
 		count++;
 	}
-	while (*s2 + j) != '\0')
+	while ((*s2 + j) != '\0')
 	{
 		j++;
 		length++;
 	}
-	for ( k = 0; k < count; k++)
-	{
-		w = s1[k];
-		total =total + w;
-	}
-	for (n = 0; n < length ; n++)
-	{
-		s = s2[n];
-		total2 = total2 + s;
-	}
-	return (total - total2);
+	s1[0] = n;
+	s2[0] = w;
+	return (n - w);
 }
