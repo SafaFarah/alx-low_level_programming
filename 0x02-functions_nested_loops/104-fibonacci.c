e02171903fba5c36ce6c319f1ad25709897c915f
SafaFarah <kangjisook13@gmail.com>
2023-03-04 11:08:16 -0800

    98 Fibonacci numbers,

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: int 0
+ */
+int main(void)
+{
+	long a = 1;
+	long b = 2;
+	long  c;
+	int count;
+
+	printf("%ld, ", a);
+	printf("%ld, ", b);
+	for (count = 0; count < 96; count++)
+	{
+		c = a + b;
+		if (count == 95)
+			printf("%ld", c);
+		else
+			printf("%ld, ", c);
+		a = b;
+		b = c
+	}
+	printf("\n");
+	return (0);
+}
