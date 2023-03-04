96bf6853f0b72da9fe40f43fa62d73700864684e
SafaFarah <kangjisook13@gmail.com>
2023-03-04 09:25:02 -0800

    the first 50 Fibonacci numbers

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,17 +7,17 @@
  */
 int main(void)
 {
-	int a = 1;
-	int b = 2;
-	int c;
+	unsigned int a = 1;
+	unsigned int b = 2;
+	unsigned int c;
 	int count;
 
-	printf("%d, ", a);
-	printf("%d, ", b);
+	printf("%u, ", a);
+	printf("%u, ", b);
 	for (count = 0; count < 48; count++)
 	{
 		c = a + b;
-		printf("%d, ", c);
+		printf("%u, ", c);
 		a = b;
 		b = c;
 	}
