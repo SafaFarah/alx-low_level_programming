5bad763b0eb830ee9fe0035ed8b910c115311007
SafaFarah <kangjisook13@gmail.com>
2023-03-13 03:53:02 -0700

    the first 98 Fibonacci numbers'

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -26,8 +26,8 @@ int main(void)
 	b2 = b % i;
 	for (count = 92; count < 99; ++count)
 	{
-		printf(", %lu", b1);
-		printf("%lu", b2);
+		printf(", %lu", b1 + (b2 / i));
+		printf("%lu", b2 % i);
 		b1 = b1 + a1;
 		a1 = b1 - a1;
 		b2 = b2 + a2;
