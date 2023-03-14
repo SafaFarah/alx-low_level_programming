a54bb4824eee439373f0df65ca329298fbca2709
SafaFarah <kangjisook13@gmail.com>
2023-03-14 10:03:52 -0700

    a function that creates an array of chars

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+# include "main.h"
+
+/**
+ * *create_array -  a function that creates an array of chars
+ * @size : size
+ * @c: char
+ *
+ * Return: char
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *s;
+	int i;
+
+	i = 0;
+	if (size == 0)
+		return (NULL);
+	while (1)
+	{
+		s = malloc(sizeof(unsigned int) * size);
+		if (s == NULL)
+			return (NULL);
+		s[0] = c;
+		i++;
+	}
+	return (0);
+}
