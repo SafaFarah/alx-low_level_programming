0fe03228a65751a9513d311a5808601f5a2c318b
root <kangjisook13@gmail.com>
2023-05-08 06:18:31 -0700

    0-function reasd and prints

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+  * read_textfile - a function that reads a text file and prints it
+  * @filename: file name
+  * @letters: number of letters it should read and print
+  *
+  * Return: actual number of letters it could read and print or 0
+  */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t of;
+	ssize_t r;
+	ssize_t p;
+	char *buff;
+
+	of = open(filename, O_RDONLY);
+	if (of == -1)
+		return (0);
+	if (filename == NULL)
+		return (0);
+	buff = malloc(sizeof(char) * letters);
+	r = read(of, buff, letters);
+	p = write(STDOUT_FILENO, buff, r);
+	if (p == -1)
+		return (0);
+	free(buff);
+	fclose(of);
+	return (p);
+}
