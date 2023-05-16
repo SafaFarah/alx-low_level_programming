1ac425ac9b945593a82ab119ab1d8117927c1ba7
SafaFarah <kangjisook13@gmail.com>
2023-05-15 22:39:54 -0700

    print_binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,7 +10,7 @@ void print_binary(unsigned long int n)
 	int count = 0;
 
 	if (n == 0)
-		_putchar(0);
+		_putchar('0');
 	while (n > 0)
 	{
 		count++;
@@ -22,8 +22,8 @@ void print_binary(unsigned long int n)
 		n = m;
 		n = n >> count;
 		if ((n & 1) == 0)
-			_putchar(0);
+			_putchar('0');
 		else
-			_putchar(1);
+			_putchar('1');
 	}
 }
