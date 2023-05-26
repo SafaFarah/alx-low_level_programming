28ada6e97bb04806a3c83eaa3a50886418c9fe40
SafaFarah <kangjisook13@gmail.com>
2023-05-25 20:53:00 -0700

    a function that appends text at the end of a file debuging

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -23,7 +23,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 
 	fo = open(filename, O_WRONLY | O_APPEND);
-	w = write(o, text_content, len);
+	w = write(fo, text_content, len);
 
 	if (fo == -1 || w == -1)
 		return (-1);
