f96fee4fbc97b03cae4cf0f89621fdebbf3a409a
SafaFarah <kangjisook13@gmail.com>
2023-08-21 17:57:00 +0000

    make me win!

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/win.so https://github.com/Zevrov/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/win.so
-export LD_PRELOAD=/tmp/win.so
+wget -O /tmp/makemewin.so https://github.com/SafaFarah/alx-low_level_programming/raw/master/0x18-dynamic_libraries/makemewin.so
+export LD_PRELOAD=/tmp/makemewin.so
