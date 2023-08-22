1546e2ce1f794032e515767932c328f62b878a69
SafaFarah <kangjisook13@gmail.com>
2023-08-22 07:33:53 +0000

    tasks 1-2 0x18-dynamic_libraries

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * add - adds two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: result of adding two number
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - substracts  two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: result of substraction two number.
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiples two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: result of multiplying two number
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: result of division two number
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - returns reminder of  two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: returns reminder of  two numbers
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
