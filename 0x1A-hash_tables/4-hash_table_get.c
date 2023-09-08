492e5470aecd1acee4539b230d1901c6cf64fa1e
SafaFarah <kangjisook13@gmail.com>
2023-09-08 21:00:25 +0000

    4-hash_table_get.c task debugging

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -20,9 +20,17 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	index = key_index((const unsigned char *)key, ht->size);
 	item = ht->array[index];
 	if (item != NULL)
+		return (NULL);
+	if (item != NULL)
 	{
 		if (strcmp(item->key, key) == 0)
 			return (item->value);
+	}
+	while (item != NULL)
+	{
+		if (strcmp(item->key, key) == 0)
+			return (item->value);
+		item = item->next;
 	}
 	return (NULL);
 }
