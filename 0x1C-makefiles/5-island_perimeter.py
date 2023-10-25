7e2b34345a3198969637a7d240b7a69aa0fee5c7
SafaFarah <kangjisook13@gmail.com>
2023-10-25 18:31:38 +0000

    0x1C-makefiles project tasks

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+finds the perimeter of the island described in grid
+"""
+def island_perimeter(grid):
+    """
+    Returns the perimeter of the island described in grid.
+    Args:
+        grid: a list of list of integers
+    """
+    return perimeter
