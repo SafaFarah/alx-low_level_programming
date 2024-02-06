c1144248b64e72435ddceec009a9eb116307e79b
SafaFarah <kangjisook13@gmail.com>
2024-02-06 09:41:07 +0200

    create file 107-O & 108-O

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -13,7 +13,7 @@ int jump_search(int *array, size_t size, int value)
 	size_t x = 0;
 	size_t i;
 
-	if (array == NULL)
+	if (array == NULL || size == 0)
 		return (-1);
 	while ((array[jump] < value) && (jump < size))
 	{
